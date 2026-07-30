4922c6da39841bf7fdd8ab55b6dbf9a12eb89306
Gabriel <gabrielhenriqueojr@gmail.com>
2026-07-30 19:16:18 -0300

    feat(auth): adiciona AuthProvider no AppProvider

--- src/components/providers/app-provider.tsx
+++ src/components/providers/app-provider.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from '@/stores/theme'
+import { AuthProvider } from '@/stores/auth'
 import { queryClient } from '@/lib/query-client'
 import { Toaster } from '@/components/ui/toaster'
 import { TooltipProvider } from '@/components/ui/tooltip'
@@ -12,12 +13,14 @@ interface AppProviderProps {
 export function AppProvider({ children }: AppProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <TooltipProvider delayDuration={200}>
-          {children}
-          <Toaster />
-        </TooltipProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <TooltipProvider delayDuration={200}>
+            {children}
+            <Toaster />
+          </TooltipProvider>
+        </ThemeProvider>
+      </AuthProvider>
     </QueryClientProvider>
   )
 }
